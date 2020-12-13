0788c8668ad8f697bb8fb85e30be874be72bb586
teejay74 <teejay744@gmail.com>
2020-12-13 12:18:27 +0500

    fixed images

--- src/js/CardWidget.js
+++ src/js/CardWidget.js
@@ -10,22 +10,6 @@ export default class CardWidget {
   }
 
   init() {
-    this.widget.innerHTML = `
-      <form id='validate_form'>
-        <div class='card-box'>
-          <div class='card' bank-id='visa'><img src='../src/img/visa.png' alt='visa'></div>
-          <div class='card' bank-id='mastercard'><img src='../src/img/mastercard.png' alt='mastercard'></div>
-          <div class='card' bank-id='american-express'><img src='../src/img/american-express.png' alt='american express'></div>
-          <div class='card' bank-id='jcb'><img src='../src/img/jcb.png' alt='jcb'></div>
-          <div class='card' bank-id='discover'><img src='../src/img/discover.png' alt='discover'></div>
-          <div class='card' bank-id='diners-club'><img src='../src/img/dinersclub.png' alt='diners club'></div>
-          <div class='card' bank-id='mir'><img src='../src/img/mir.png' alt='mir'></div>
-        </div>
-        <div class='input-box'>
-          <input id='validate_input' type='text'>
-          <button id='validate_button'>Click to Validate</button>
-        </div>
-     </form>`;
     this.bankImg = document.getElementsByClassName('card');
     this.input = document.getElementById('validate_input');
     const form = document.getElementById('validate_form');
